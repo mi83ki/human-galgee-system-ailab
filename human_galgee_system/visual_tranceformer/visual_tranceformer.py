b0d722d2359aa60a5af42ab74dc5296a023cf689
Tatsuya Miyazaki <miyazaki.tatsuya.mail@gmail.com>
2025-02-16 11:16:35 +0900

    Merge BishoujoWorld.py

--- human_galgee_system/visual_tranceformer/visual_tranceformer.py
+++ human_galgee_system/visual_tranceformer/visual_tranceformer.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import argparse
 import copy
+import threading
+import time
 from pathlib import Path
 
 import cv2 as cv
 import mediapipe as mp
 import webuiapi
-from PIL import Image
+from PIL import Image, ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 from human_galgee_system.utils import CvFpsCalc
 
@@ -15,9 +19,29 @@ FACETRIM_IMAGE_PATH = f"{IMAGE_DIR}facetrim.png"
 CONVERTED_IMAGE_PATH = f"{IMAGE_DIR}girlimage.png"
 
 
+# StableDiffusionのimg2imgで画像を生成する
+def conv_face2girl(api, prompt, faceimage):
+    # 画像を生成する
+    # faceimage = Image.open(FACETRIM_IMAGE_PATH)
+    girlimage = api.img2img(
+        images=[faceimage],
+        prompt=prompt,
+        seed=5555,
+        cfg_scale=6.5,
+        denoising_strength=0.4,
+    )
+    girlimage.image.save(CONVERTED_IMAGE_PATH)
+    # 一定時間待つ
+    time.sleep(1)
+
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--prompt", type=str, default="beautiful girl,shinkai makoto")
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        default="super fine illustration, best quality, anime screencap, cowboy shot, 1 girl, brown hair, basketball court, team uniform, realistic, beautiful, anime, anime faces",
+    )
     parser.add_argument("--device", type=int, default=0)
     parser.add_argument("--width", help="cap width", type=int, default=960)
     parser.add_argument("--height", help="cap height", type=int, default=540)
@@ -71,6 +95,10 @@ def main():
     # FPS計測モジュール ########################################################
     cvFpsCalc = CvFpsCalc(buffer_len=10)
 
+    thread = None
+    mode = None
+    posX, posY, posCX, posCY, sizeW, sizeH = 0, 0, 0, 0, 0, 0
+
     while True:
         # カメラキャプチャ　###############################################################
         ret, image = cap.read()
@@ -92,19 +120,50 @@ def main():
                 )
                 # 顔の切り抜き画像を取得
                 faceimage = trim_face(posX, posY, sizeW, sizeH, image)
-                # StableDiffusion変換後画像を取得
-                sdimage = conv_face2girl(api, faceimage, prompt)
-                # StableDiffusion返還後画像を重ねる
-                overlay_image = overlay_illust(image, sdimage, posCX, posCY, sizeH)
+                # thread開始
+                if (
+                    thread is None or not thread.is_alive()
+                ):  # ThreadがNoneもしくはThreadが動いてなかったら
+                    faceimage = Image.open(FACETRIM_IMAGE_PATH)
+                    print("thread start")
+                    thread = threading.Thread(
+                        target=conv_face2girl,
+                        args=(api, prompt, faceimage),
+                    )
+                    thread.start()
 
         # キー処理(ESC：終了) #################################################
         key = cv.waitKey(1)
         if key == 27:  # ESC
             break
-
+        if key == ord("1"):  # 美少女モード
+            prompt = "a young girl with detailed reflecting eyes by professional digital painting in granblue fantasy style, beautiful pretty cute face, full body shot of loli anime girl, a small smile, short blonde hair, big ribbon on the head, wearing fantasy clothes, front lighting, 8k resolution, featured on pixiv"
+            mode = "All Human Girls Mode"
+        elif key == ord("2"):  # 美少年モード
+            prompt = "super fine illustration, best quality, anime screencap, cowboy shot, 1 girl, brown hair, basketball court, team uniform, realistic, beautiful, anime, anime faces"
+            mode = "All Human Boys Mode"
+        elif key == ord("3"):  # おばあちゃんモード
+            prompt = "masterpiece,high quality,(elder woman),a photo of female"
+            mode = "All Human Obaa-Chan Mode"
+        elif key == ord("4"):  # おじいちゃんモード
+            prompt = "masterpiece,high quality,(elder man),a photo of male"
+            mode = "All Human Ojii-Chan Mode"
+        # StableDiffusion返還後画像を重ねる
+        cv.putText(
+            image,
+            mode,
+            (10, 30),
+            cv.FONT_HERSHEY_SIMPLEX,
+            1.0,
+            (0, 255, 0),
+            2,
+            cv.LINE_AA,
+        )
+        overlay_image = overlay_illust(image, posCX, posCY, sizeH)
         # 画面反映 #############################################################
-        cv.imshow("MediaPipe Face Detection Demo", overlay_image)
-
+        cv.namedWindow("window", cv.WINDOW_NORMAL)
+        cv.resizeWindow("window", 1140, 960)
+        cv.imshow("window", overlay_image)
     cap.release()
     cv.destroyAllWindows()
 
@@ -120,85 +179,59 @@ def culculate_face_pos_and_size(image, detection):
     posCX = int(bbox.xmin * image_width + (sizeW / 2))  # X中心座標
     posCY = int(bbox.ymin * image_height + (sizeH / 2))  # Y中心座標
 
-    cv.putText(
-        image,
-        "posX:"
-        + str(posX)
-        + " posY:"
-        + str(posY)
-        + " sizeW"
-        + str(sizeW)
-        + " sizeH"
-        + str(sizeH),
-        (10, 30),
-        cv.FONT_HERSHEY_SIMPLEX,
-        1.0,
-        (0, 255, 0),
-        2,
-        cv.LINE_AA,
-    )
+    # cv.putText(image, "posX:" + str(posX) + " posY:" + str(posY) + " sizeW" + str(sizeW) + " sizeH" + str(sizeH),
+    #           (10,30),cv.FONT_HERSHEY_SIMPLEX,1.0,(0,255,0),2,cv.LINE_AA)
 
     return image, posX, posY, posCX, posCY, sizeW, sizeH
 
 
 # 重ね合わせ画像をresizeして透明化して重ねる
-def overlay_illust(bg, ol, posX, posY, sizeH):
-    olimage = cv.imread(CONVERTED_IMAGE_PATH, cv.IMREAD_UNCHANGED)
-    resize_ol_image = cv.resize(olimage, dsize=None, fx=0.6, fy=0.6)
-    resize_ol_image_height = resize_ol_image.shape[0]
-    resize_ol_image_width = resize_ol_image.shape[1]
+def overlay_illust(bg, posX, posY, sizeH):
+    try:
+        olimage = cv.imread(CONVERTED_IMAGE_PATH, cv.IMREAD_UNCHANGED)
+        resize_ol_image = cv.resize(
+            olimage,
+            dsize=None,
+            fx=sizeH * 0.0035,
+            fy=sizeH * 0.0035,
+        )
+        resize_ol_image_height = resize_ol_image.shape[0]
+        resize_ol_image_width = resize_ol_image.shape[1]
 
-    # 重ね合わせ画像のアルファチャンネルだけ抜き出す(0~255の値が入っている)
-    # alpha = resize_ol_image[:,:,3]
-    # alpha = cv.cvtColor(alpha, cv.COLOR_GRAY2BGR) # grayをBGRに変換(各ピクセルのα値を各チャンネル(B,G,Rにコピー))
-    # alpha = alpha /255.0 #0.0 ~ 1.0の間に変換
+        posY = posY - 50  # 高さ方向のオフセット
 
-    # laugh_man_color = resize_ol_image[:,:,:3] #色情報のみを抜き出す
-
-    # カメラ映像に重ね合わせ画像が入りきる場合は重ね合わせ
-    if (
-        (posY - (resize_ol_image_height / 2) > 0)
-        & (posY + (resize_ol_image_height / 2) < bg.shape[0])
-        & (posX - (resize_ol_image_width / 2) > 0)
-        & (posX + (resize_ol_image_width / 2) < bg.shape[1])
-    ):
-        # bg[int(posY-(resize_ol_image_height/2)):int(posY+(resize_ol_image_height/2)),int(posX-(resize_ol_image_width/2)):int(posX+(resize_ol_image_width/2))] = (bg[int(posY-(resize_ol_image_height/2)):int(posY+(resize_ol_image_height/2)),int(posX-(resize_ol_image_width/2)):int(posX+(resize_ol_image_width/2))] * (1.0 - alpha)).astype('uint8') #透明度がMaxの箇所はBGR値を0に(黒に)
-        # bg[int(posY-(resize_ol_image_height/2)):int(posY+(resize_ol_image_height/2)),int(posX-(resize_ol_image_width/2)):int(posX+(resize_ol_image_width/2))] = (bg[int(posY-(resize_ol_image_height/2)):int(posY+(resize_ol_image_height/2)),int(posX-(resize_ol_image_width/2)):int(posX+(resize_ol_image_width/2))] + (laugh_man_color * alpha)).astype('uint8') #合成
-        bg[
-            int(posY - (resize_ol_image_height / 2)) : int(
-                posY + (resize_ol_image_height / 2),
-            ),
-            int(posX - (resize_ol_image_width / 2)) : int(
-                posX + (resize_ol_image_width / 2),
-            ),
-        ] = resize_ol_image
-    return bg
+        # カメラ映像に重ね合わせ画像が入りきる場合は重ね合わせ
+        if (
+            (posY - (resize_ol_image_height / 2) > 0)
+            & (posY + (resize_ol_image_height / 2) < bg.shape[0])
+            & (posX - (resize_ol_image_width / 2) > 0)
+            & (posX + (resize_ol_image_width / 2) < bg.shape[1])
+        ):
+            # bg[int(posY-(resize_ol_image_height/2)):int(posY+(resize_ol_image_height/2)),int(posX-(resize_ol_image_width/2)):int(posX+(resize_ol_image_width/2))] = (bg[int(posY-(resize_ol_image_height/2)):int(posY+(resize_ol_image_height/2)),int(posX-(resize_ol_image_width/2)):int(posX+(resize_ol_image_width/2))] * (1.0 - alpha)).astype('uint8') #透明度がMaxの箇所はBGR値を0に(黒に)
+            # bg[int(posY-(resize_ol_image_height/2)):int(posY+(resize_ol_image_height/2)),int(posX-(resize_ol_image_width/2)):int(posX+(resize_ol_image_width/2))] = (bg[int(posY-(resize_ol_image_height/2)):int(posY+(resize_ol_image_height/2)),int(posX-(resize_ol_image_width/2)):int(posX+(resize_ol_image_width/2))] + (laugh_man_color * alpha)).astype('uint8') #合成
+            bg[
+                int(posY - (resize_ol_image_height / 2)) : int(
+                    posY + (resize_ol_image_height / 2),
+                ),
+                int(posX - (resize_ol_image_width / 2)) : int(
+                    posX + (resize_ol_image_width / 2),
+                ),
+            ] = resize_ol_image
+        return bg
+    except Exception as ex:
+        print(ex)
+        return bg
 
 
-# 顔の部分を切り抜き
+# 顔の部分を切り抜き()
 def trim_face(posX, posY, sizeW, sizeH, image):
-    faceimage = image[posY : posY + sizeH, posX : posX + sizeW]
-    cv.imwrite(FACETRIM_IMAGE_PATH, faceimage)
-    return faceimage
-
-
-# StableDiffusionのimg2imgで画像を生成する
-def conv_face2girl(api, faceimage, prompt):
-    # 画像を生成する
-    faceimage = Image.open(FACETRIM_IMAGE_PATH)
-    girlimage = api.img2img(
-        images=[faceimage],
-        prompt=prompt,
-        seed=5555,
-        cfg_scale=6.5,
-        denoising_strength=0.2,
-    )
-    girlimage.image.save(CONVERTED_IMAGE_PATH)
-    # if girlimage is None:
-    #    laugh_man = cv.imread("C:\\Users\\user\\Desktop\\HearExpansion\\HumanGalgeeSystem\\Development\\HumanGalgeeSystem\\warai_flat.png",cv.IMREAD_UNCHANGED)  # アルファチャンネル込みで読み込む)
-    #    girlimage = laugh_man
-
-    return girlimage
+    try:
+        faceimage = image[posY - 100 : posY + sizeH, posX - 50 : posX + sizeW + 50]
+        cv.imwrite(FACETRIM_IMAGE_PATH, faceimage)
+        return faceimage
+    except Exception:
+        # print(ex)
+        return image
 
 
 if __name__ == "__main__":
